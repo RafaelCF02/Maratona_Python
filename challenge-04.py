b36ef64a3ca5dc364905629dd5f9fb95b977781e
RafaelCF02 <rafaelc1002@gmail.com>
2025-01-12 19:05:32 -0300

    Desafio atualizado

--- challenge-04.py
+++ challenge-04.py
@@ -1,25 +1,41 @@
 import requests
 
-urls = str(input("Insira os sites a serem verificados separados por vírgula: ")).lower().split(",")
+def menu():
+    opcao = input("Deseja inserir mais urls? [s/n]:  ").lower()
 
-for url in urls:
-    url = url.strip()
-    if "." not in url:
-        print(f"{url} Site inválido")
-
+    if opcao[0] == "s":
+        main()
+    
+    elif opcao[0] == "n":
+        print("Fim!")
+        return
     else:
-        if "http://" not in url:
-            url = f"http://{url}"
+        print("opção Invalida, Insira novamente")
+        menu()
 
-        try:
+def main():
 
-            status = requests.get(url, timeout=5).status_code
-            if status == 200:
-                print(f"{url} Site Ativo!")
-            else:
-                print(f"{url} Site Offline!")
+    urls = str(input("Insira os sites a serem verificados separados por vírgula: ")).lower().split(",")
 
-        except:
-        
-            print(f"Erro ao acessar {url}")
-       
+    for url in urls:
+        url = url.strip()
+        if "." not in url:
+            print(f"{url} Site inválido")
+
+        else:
+            if "http://" not in url:
+                url = f"http://{url}"
+
+            try:
+
+                status = requests.get(url, timeout=5).status_code
+                if status == 200:
+                    print(f"{url} Site Ativo!")
+                else:
+                    print(f"{url} Site Offline!")
+
+            except:
+            
+                print(f"Erro ao acessar {url}")
+    menu()
+main()
